7cb5eef49ac5997859281607982d61753120f751
ncosgray <ncosgray@gmail.com>
2022-04-27 18:56:41 -0700

    initial commit of version 1.1.2

---
+++ app/src/main/java/com/nathanatos/kolexchangewidget/KoLExchangeWidget.java
@@ -0,0 +1,101 @@
+package com.nathanatos.kolexchangewidget;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetProvider;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.widget.RemoteViews;
+
+public class KoLExchangeWidget extends AppWidgetProvider {
+
+	private static final String KOLEXCHANGE_WS_URL = "https://www.nathanatos.com/kol/ws_getrate.php";
+	private static final String KOLEXCHANGE_WS_NODE = "rate";
+	private static final String KOLEXCHANGE_LABEL = "$1 US = ";
+	private static final String KOLEXCHANGE_CLICK_URL = "https://www.nathanatos.com/kol/";
+	private static final String KOLEXCHANGE_CLICK = "KoLWidgetClicked";
+	
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		
+		// set up the click intent
+		ComponentName thisWidget = new ComponentName(context, KoLExchangeWidget.class);
+		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main);
+		Intent intent = new Intent(context, KoLExchangeWidget.class);
+		intent.setAction(KOLEXCHANGE_CLICK);
+		views.setOnClickPendingIntent(R.id.widget_rootview, PendingIntent.getBroadcast(context, 0, intent, 0));
+		appWidgetManager.updateAppWidget(thisWidget, views);
+		
+		// do the data update in another thread
+		new WidgetUpdateTask().execute(context);
+		
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	    super.onReceive(context, intent);
+
+	    // process a user click on the widget
+	    if (intent.getAction() != null && intent.getAction().equals(KOLEXCHANGE_CLICK)) {
+	        try {
+	            Intent webIntent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(KOLEXCHANGE_CLICK_URL));
+	            webIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	            context.startActivity(webIntent);
+	        } catch (RuntimeException e) {
+	            e.printStackTrace();
+	        }
+	        
+			// do an extra data update in another thread
+			new WidgetUpdateTask().execute(context);
+			
+		}
+	}
+	
+	private class WidgetUpdateTask extends AsyncTask<Context, Void, String> {
+		
+		private Context context;
+		
+		protected String doInBackground(Context... params) {
+
+			// get context
+			context = params[0];
+			
+			// load data from web service
+			String updateText = null;
+	    	XMLParser parser = new XMLParser();
+			String xml = parser.getXmlFromUrl(KOLEXCHANGE_WS_URL);
+			if(xml != null) {
+				Document doc = parser.getDomElement(xml);
+				if(doc != null) {
+					NodeList nl = doc.getElementsByTagName(KOLEXCHANGE_WS_NODE);
+					if (nl.getLength() > 0) {
+						updateText = KOLEXCHANGE_LABEL + parser.getElementValue(nl.item(0));
+					}
+				}
+			}
+			return updateText;
+			
+	    }
+
+	    protected void onPostExecute(String resultText) {
+	    	
+			// update the widget text only if a value was received
+	    	if(resultText != null) {
+	    		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+	    		RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.main);
+	    		ComponentName thisWidget = new ComponentName(context, KoLExchangeWidget.class);
+	    		updateViews.setTextViewText(R.id.widget_textview, resultText);
+	    		appWidgetManager.updateAppWidget(thisWidget, updateViews);
+	    	}
+	    
+	    }
+
+	}
+}
+
---
+++ app/src/main/java/com/nathanatos/kolexchangewidget/XMLParser.java
@@ -0,0 +1,106 @@
+package com.nathanatos.kolexchangewidget;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import android.util.Log;
+
+public class XMLParser {
+
+	// Cribbed from:
+	// http://www.androidhive.info/2011/11/android-xml-parsing-tutorial/
+	// and http://androidcookbook.com/Recipe.seam?recipeId=79
+	
+	public String getXmlFromUrl(String xmlurl) {
+        String xml = null;
+ 
+        try {
+        	// use URLConnection to fetch data
+        	URL url = new URL(xmlurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(10000);
+            conn.setReadTimeout(10000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+    	
+            StringBuilder sb = new StringBuilder();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+            }
+            in.close();
+            conn.disconnect(); 
+            xml = sb.toString();
+ 
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // return XML
+        return xml;
+    }
+	
+	public Document getDomElement(String xml){
+        Document doc = null;
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+ 
+            DocumentBuilder db = dbf.newDocumentBuilder();
+ 
+            InputSource is = new InputSource();
+                is.setCharacterStream(new StringReader(xml));
+                doc = db.parse(is); 
+ 
+            } catch (ParserConfigurationException e) {
+                Log.e("Error: ", e.getMessage());
+                return null;
+            } catch (SAXException e) {
+                Log.e("Error: ", e.getMessage());
+                return null;
+            } catch (IOException e) {
+                Log.e("Error: ", e.getMessage());
+                return null;
+            }
+            // return DOM
+            return doc;
+    }
+	
+	public String getValue(Element item, String str) {
+	    NodeList n = item.getElementsByTagName(str);
+	    return this.getElementValue(n.item(0));
+	}
+	 
+	public final String getElementValue( Node elem ) {
+	         Node child;
+	         if( elem != null){
+	             if (elem.hasChildNodes()){
+	                 for( child = elem.getFirstChild(); child != null; child = child.getNextSibling() ){
+	                     if( child.getNodeType() == Node.TEXT_NODE  ){
+	                         return child.getNodeValue();
+	                     }
+	                 }
+	             }
+	         }
+	         return "";
+	  } 
+	
+}
