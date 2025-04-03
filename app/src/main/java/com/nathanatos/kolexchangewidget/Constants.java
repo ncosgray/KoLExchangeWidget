8d35115a7410ecf277e893a6ca55a9fdb589817f
ncosgray <ncosgray@gmail.com>
2025-04-03 12:11:19 -0700

    change to new data API and graph

--- app/src/main/java/com/nathanatos/kolexchangewidget/Constants.java
+++ app/src/main/java/com/nathanatos/kolexchangewidget/Constants.java
@@ -4,7 +4,7 @@
  Class:    Constants.java
  Author:   Nathan Cosgray | https://www.nathanatos.com
  -------------------------------------------------------------------------------
- Copyright (c) 2013-2024 Nathan Cosgray. All rights reserved.
+ Copyright (c) 2013-2025 Nathan Cosgray. All rights reserved.
  This source code is licensed under the BSD-style license found in LICENSE.txt.
  *******************************************************************************
 */
@@ -18,11 +18,11 @@ import java.time.Duration;
 public class Constants {
 
     // Exchange data constants
-    public static final String KOLEXCHANGE_WS_URL = "https://www.nathanatos.com/kol/ws_getrate.php";
+    public static final String KOLEXCHANGE_WS_URL = "https://4hea44d1a5.execute-api.us-east-1.amazonaws.com/getrate?xml=true";
     public static final String KOLEXCHANGE_WS_NODE = "rate";
     public static final String KOLEXCHANGE_LABEL = "$1 US = ";
-    public static final String KOLEXCHANGE_GRAPH_URL = "https://www.nathanatos.com/kol/rate_history_1mo.png";
-    public static final int KOLEXCHANGE_TIMEOUT = 2500; // milliseconds
+    public static final String KOLEXCHANGE_GRAPH_URL = "https://kol-exchange-web.s3.amazonaws.com/rate_history_1mo.png";
+    public static final int KOLEXCHANGE_TIMEOUT = 5000; // milliseconds
     public static final int KOLEXCHANGE_RETRIES = 3;
 
     // Main activity
