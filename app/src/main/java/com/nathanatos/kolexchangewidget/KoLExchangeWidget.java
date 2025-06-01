49ba39d2cfdc19e881f98f45a55baa03af2c4b58
ncosgray <ncosgray@gmail.com>
2025-06-01 11:00:55 -0700

    bump copyright year

--- app/src/main/java/com/nathanatos/kolexchangewidget/KoLExchangeWidget.java
+++ app/src/main/java/com/nathanatos/kolexchangewidget/KoLExchangeWidget.java
@@ -4,7 +4,7 @@
  Class:    KoLExchangeWidget.java
  Author:   Nathan Cosgray | https://www.nathanatos.com
  -------------------------------------------------------------------------------
- Copyright (c) 2013-2024 Nathan Cosgray. All rights reserved.
+ Copyright (c) 2013-2025 Nathan Cosgray. All rights reserved.
  This source code is licensed under the BSD-style license found in LICENSE.txt.
  *******************************************************************************
 */
